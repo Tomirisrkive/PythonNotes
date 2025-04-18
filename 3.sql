c5075853ddef8e265d6ac82a38c8185a63f57019
Tomiris <zharylkasyntomiris1@gmail.com>
2025-04-18 22:50:25 +0500

    lab11

--- 3.sql
+++ 3.sql
@@ -10,7 +10,7 @@ INSERT INTO phonebook1 (username, phone_number)
 VALUES
   ('Tomiris', '123456789'),
   ('John Doe', '987654321'),
-  ('Alice Smith', '5556667780');
+  ('Alice Smith', '5556667789');
 
 -- 3. Функция для поиска по шаблону
 CREATE OR REPLACE FUNCTION search_records(pattern TEXT)
